caaaf7d2ab9d759e78664186b9f1030c90fb409c
kaifeng123 <1799283315@qq.com>
2021-04-11 18:03:10 +0800

    第一次提交常见Cube随鼠标移动。局域联网

---
+++ Assets/Data/Script/CubeMove.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mirror;
+public class CubeMove : NetworkBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!isLocalPlayer)
+            return;
+        if(Input.GetMouseButton(0))
+        {
+         Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            mousePosition.z = transform.position.z;
+            transform.position = mousePosition;
+        }
+    }
+}
